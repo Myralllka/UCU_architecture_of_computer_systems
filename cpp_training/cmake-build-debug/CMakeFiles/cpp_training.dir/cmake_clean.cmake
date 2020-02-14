367374531144ef35ddb67aa0bcefc19a286222ab
Mykola Morgunenko <Olli25010@gmail.com>
2020-02-14 15:16:58 +0200

    add two training projects

---
+++ cpp_training/cmake-build-debug/CMakeFiles/cpp_training.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_training.dir/main.cpp.o"
+  "cpp_training"
+  "cpp_training.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_training.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
