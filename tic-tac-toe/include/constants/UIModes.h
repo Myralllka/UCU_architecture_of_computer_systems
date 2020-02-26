d5eb6c84dd8ff92390a26cffa879d3c683b4a1eb
Mykola Morgunenko <Olli25010@gmail.com>
2020-02-26 23:30:36 +0200

    add new

---
+++ tic-tac-toe/include/constants/UIModes.h
@@ -0,0 +1,15 @@
+//
+// Created by fenix on 2/22/20.
+//
+
+#ifndef TIC_TAC_TOE_UIMODES_H
+#define TIC_TAC_TOE_UIMODES_H
+
+
+enum class UIModes : char {
+    console = 'c';
+    visual = 'v';
+};
+
+
+#endif //TIC_TAC_TOE_UIMODES_H
