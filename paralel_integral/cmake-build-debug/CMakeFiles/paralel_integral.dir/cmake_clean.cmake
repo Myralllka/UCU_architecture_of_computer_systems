8444fd64d03e332210d41c20e81b7a172d8a12bd
Mykola Morgunenko <Olli25010@gmail.com>
2020-02-28 11:43:47 +0200

    add struct of vars

---
+++ paralel_integral/cmake-build-debug/CMakeFiles/paralel_integral.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paralel_integral.dir/main.cpp.o"
+  "paralel_integral"
+  "paralel_integral.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paralel_integral.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
