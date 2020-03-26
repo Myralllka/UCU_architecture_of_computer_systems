6eb91ff044d442da5f1bf5456383e5464968ce1d
Mykola Morgunenko <Olli25010@gmail.com>
2020-03-26 15:57:00 +0200

    add extractor

---
+++ count_number_of_all_words/includes/linear_program.h
@@ -0,0 +1,12 @@
+//
+// Created by myralllka on 3/25/20.
+//
+
+#ifndef COUNT_NUMBER_OF_ALL_WORDS_LINEAR_PROGRAM_H
+#define COUNT_NUMBER_OF_ALL_WORDS_LINEAR_PROGRAM_H
+
+#include <string>
+
+void count_words (std::string &input_filename, std::string &output_filename_a, std::string &output_filename_n);
+
+#endif //COUNT_NUMBER_OF_ALL_WORDS_LINEAR_PROGRAM_H
