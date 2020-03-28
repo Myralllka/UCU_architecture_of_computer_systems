aa1bfa2b9553a0444b26d3c706193fdcd7501abd
Mykola Morgunenko <Olli25010@gmail.com>
2020-03-28 12:52:40 +0200

    EXTRACTOR WORKS

--- count_number_of_all_words/includes/extractor.h
+++ count_number_of_all_words/includes/extractor.h
@@ -1,5 +1,6 @@
 //
 // Created by myralllka on 3/26/20.
+// Code from https://github.com/libarchive/libarchive/wiki/Examples#A_Universal_Decompressor
 //
 
 #ifndef COUNT_NUMBER_OF_ALL_WORDS_EXTRACTOR_H
@@ -8,34 +9,15 @@
 #include <iostream>
 #include <archive.h>
 #include <archive_entry.h>
+#include <vector>
 
-
-static int copy_data(struct archive *ar, struct archive *aw) {
-    int r;
-    const void *buff;
-    size_t size;
-    off_t offset;
-    for (;;) {
-        r = archive_read_data_block(ar, &buff, &size, &offset);
-        if (r == ARCHIVE_EOF)
-            return (ARCHIVE_OK);
-        if (r < ARCHIVE_OK)
-            return (r);
-        r = archive_write_data_block(aw, buff, size, offset);
-        if (r < ARCHIVE_OK) {
-            fprintf(stderr, "%s\n", archive_error_string(aw));
-            return (r);
-        }
-    }
-}
-
-static void extract_to_memory(std::string& buffer, std::string output) {
-    struct archive *a = archive_read_new();
+static std::string extract_to_memory(std::string &buffer) {
+    struct archive *a;
     struct archive *ext;
     struct archive_entry *entry;
     int flags;
     int r;
-    size_t _used;
+    off_t filesize;
 
     /* Select which attributes we want to restore. */
     flags = ARCHIVE_EXTRACT_TIME;
@@ -43,8 +25,9 @@ static void extract_to_memory(std::string& buffer, std::string output) {
     flags |= ARCHIVE_EXTRACT_ACL;
     flags |= ARCHIVE_EXTRACT_FFLAGS;
 
-    archive_read_support_filter_all(a);
+    a = archive_read_new();
     archive_read_support_format_all(a);
+    archive_read_support_filter_all(a);
     ext = archive_write_disk_new();
     archive_write_disk_set_options(ext, flags);
     archive_write_disk_set_standard_lookup(ext);
@@ -52,20 +35,30 @@ static void extract_to_memory(std::string& buffer, std::string output) {
         exit(1);
     for (;;) {
         r = archive_read_next_header(a, &entry);
-        if (r == ARCHIVE_EOF) break;
-        if (r < ARCHIVE_OK) fprintf(stderr, "%s\n", archive_error_string(a));
-        if (r < ARCHIVE_WARN) exit(1);
-//        r = archive_write_open_memory(a, (void *) output.c_str(), output.size(), &_used);
+        if (r == ARCHIVE_EOF)
+            break;
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(a));
+        if (r < ARCHIVE_WARN)
+            exit(1);
         r = archive_write_header(ext, entry);
-        if (r < ARCHIVE_OK) fprintf(stderr, "%s\n", archive_error_string(ext));
+        filesize = archive_entry_size(entry);
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
         else if (archive_entry_size(entry) > 0) {
-            r = copy_data(a, ext);
-            if (r < ARCHIVE_OK) fprintf(stderr, "%s\n", archive_error_string(ext));
-            if (r < ARCHIVE_WARN) exit(1);
+            std::string output (filesize, char{});
+            r = archive_read_data(a, &output[0], output.size());
+            std::cout << output << std::endl;
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(ext));
+            if (r < ARCHIVE_WARN)
+                exit(1);
         }
         r = archive_write_finish_entry(ext);
-        if (r < ARCHIVE_OK) fprintf(stderr, "%s\n", archive_error_string(ext));
-        if (r < ARCHIVE_WARN) exit(1);
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
+        if (r < ARCHIVE_WARN)
+            exit(1);
     }
     archive_read_close(a);
     archive_read_free(a);
