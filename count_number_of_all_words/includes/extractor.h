090ee1bcd1f03e0197a91c4b9f1e33afc455c923
Mykola Morgunenko <Olli25010@gmail.com>
2020-03-29 07:19:45 +0300

    EXTRACTOR WORKS

--- count_number_of_all_words/includes/extractor.h
+++ count_number_of_all_words/includes/extractor.h
@@ -13,24 +13,12 @@
 
 static std::string extract_to_memory(std::string &buffer) {
     struct archive *a;
-    struct archive *ext;
     struct archive_entry *entry;
-    int flags;
     int r;
     off_t filesize;
-
-    /* Select which attributes we want to restore. */
-    flags = ARCHIVE_EXTRACT_TIME;
-    flags |= ARCHIVE_EXTRACT_PERM;
-    flags |= ARCHIVE_EXTRACT_ACL;
-    flags |= ARCHIVE_EXTRACT_FFLAGS;
-
     a = archive_read_new();
     archive_read_support_format_all(a);
     archive_read_support_filter_all(a);
-    ext = archive_write_disk_new();
-    archive_write_disk_set_options(ext, flags);
-    archive_write_disk_set_standard_lookup(ext);
     if ((r = archive_read_open_memory(a, buffer.c_str(), buffer.size())))
         exit(1);
     for (;;) {
@@ -41,29 +29,15 @@ static std::string extract_to_memory(std::string &buffer) {
             fprintf(stderr, "%s\n", archive_error_string(a));
         if (r < ARCHIVE_WARN)
             exit(1);
-        r = archive_write_header(ext, entry);
         filesize = archive_entry_size(entry);
-        if (r < ARCHIVE_OK)
-            fprintf(stderr, "%s\n", archive_error_string(ext));
-        else if (archive_entry_size(entry) > 0) {
-            std::string output (filesize, char{});
-            r = archive_read_data(a, &output[0], output.size());
-            std::cout << output << std::endl;
-            if (r < ARCHIVE_OK)
-                fprintf(stderr, "%s\n", archive_error_string(ext));
-            if (r < ARCHIVE_WARN)
-                exit(1);
-        }
-        r = archive_write_finish_entry(ext);
-        if (r < ARCHIVE_OK)
-            fprintf(stderr, "%s\n", archive_error_string(ext));
+        std::string output(filesize, char{});
+        r = archive_read_data(a, &output[0], output.size());
+        std::cout << output << std::endl;
         if (r < ARCHIVE_WARN)
             exit(1);
     }
     archive_read_close(a);
     archive_read_free(a);
-    archive_write_close(ext);
-    archive_write_free(ext);
     exit(0);
 }
 
