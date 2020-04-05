a88371350f050bb604251ddbd471df2d783b48a5
solka13Lenio <solomialenio13@gmail.com>
2020-04-05 21:07:03 +0300

    script min time

--- count_number_of_all_words/start.py
+++ count_number_of_all_words/start.py
@@ -1,4 +1,3 @@
-#!venv/bin python3
 import sys, os
 import re
 
@@ -18,20 +17,26 @@ def build(debug):
 
 
 def run(n, config_file):
+    durations = list()
+
     print("..runing program")
     os.chdir("cmake-build-debug")
     for _ in range(n):
-        os.system("./count_number_of_all_words ../{}".format(config_file))
+        os.system("./count_number_of_all_words ../{} >> durations.txt".format(config_file))
+        with open("durations.txt", 'r') as dur_file:
+            durations.append(float(re.match(r"Total: \d+.\d+", dur_file.readlines()[-1]).group(0)))
+    os.system("rm durations.txt")
     os.chdir("../")
+    print("> minimum runtime {}".format(min(durations)))
 
 
 def check_results(config_file):
     print("..checking results")
     # run in 1 thread
     # create new config file
-    with open(config_file, r) as main_config:
+    with open(config_file, 'r') as main_config:
         infile = main_config.readline()[9:]
-    with open("test_config.conf", r) as test_config:
+    with open("test_config.conf", 'r') as test_config:
         test_config.write("""infile = {}
 out_by_a = test_res_a.txt
 out_by_n = test_res_n.txt
