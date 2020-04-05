4f737e31634332d6537a37cc6edfe757187d3f76
Mykola Morgunenko <Olli25010@gmail.com>
2020-04-05 15:06:56 +0300

    add linear program writing to files

--- count_number_of_all_words/src/linear_program.cpp
+++ count_number_of_all_words/src/linear_program.cpp
@@ -8,21 +8,7 @@
 #include <map>
 #include <string>
 #include <boost/locale.hpp>
-
-template<typename A, typename B>
-std::pair<B, A> flip_pair(const std::pair<A, B> &p) {
-    return std::pair<B, A>(p.second, p.first);
-}
-
-template<typename A, typename B, template<class, class, class...> class M, class... Args>
-std::multimap<B, A> flip_map(const M<A, B, Args...> &src) {
-    std::multimap<B, A> dst;
-    std::transform(src.begin(), src.end(),
-                   std::inserter(dst, dst.begin()),
-                   flip_pair<A, B>);
-    return dst;
-}
-
+#include <algorithm>
 
 void count_words(const std::string &input_filename, const std::string &output_filename_a,
                  const std::string &output_filename_n) {
