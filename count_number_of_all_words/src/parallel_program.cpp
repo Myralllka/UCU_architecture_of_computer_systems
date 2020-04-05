6b65df715934a051188e88a8ba361f59fc2a6701
solka13Lenio <solomialenio13@gmail.com>
2020-04-05 22:07:45 +0300

    index one file only

--- count_number_of_all_words/src/parallel_program.cpp
+++ count_number_of_all_words/src/parallel_program.cpp
@@ -16,21 +16,22 @@
 #include <boost/locale.hpp>
 #include <thread>
 #include <cmath>
-#include <set>
-#include <algorithm>
-#include <functional>
 
-void count_words(std::vector<std::string> &data, const int start_position, const int end_position,
+
+void count_words(std::string &data, const int start_position, const int end_position,
                  std::map<std::string, int> &map_of_words, t_queue<std::map<std::string, int>> &queue) {
-    namespace bl =  boost::locale;
-
+    int start = start_position;
+    int end = end_position;
+    if (start_position - 1 > 0 && !isspace(data[start_position - 1])) {
+        while (!isspace(data[start_position - 1])) {
+            start--;
+        }
+    }
+    while (!isspace(data[end_position - 1])) {
+        end--;
+    }
     std::string word;
-    for (auto cur = data.begin() + start_position; cur != data.begin() + end_position; cur++) {
-        std::string element = *cur;
-        element = bl::to_lower(bl::fold_case(bl::normalize(element)));
-        element.erase(std::remove_if(element.begin(), element.end(),
-                                     [](const unsigned &c) { return !isspace(c) && !isalpha(c); }), element.end());
-        for (auto &chr : element) {
+    for (auto &chr : data.substr(start_position, end_position)) {
             if (isalpha(chr))
                 word += tolower(chr);
             else if (isspace(chr)) {
@@ -43,8 +44,7 @@ void count_words(std::vector<std::string> &data, const int start_position, const
                 word.clear();
             }
         }
-    }
-    queue.emplace_back(merge_maps(map_of_words, map_of_words));
+//    queue.emplace_back(merge_maps(map_of_words, map_of_words));
 }
 
 void merge_maps_queue(t_queue<std::map<std::string, int>> &queue) {
@@ -69,10 +69,18 @@ void parallel_count(const std::string &input_filename, const std::string &output
         return ss.str();
     }();
     extract_to_memory(buffer, &data);
+    std::string file_data = data[0];
+
+    namespace bl =  boost::locale;
+
+    file_data = bl::to_lower(bl::fold_case(bl::normalize(file_data)));
+    file_data.erase(std::remove_if(file_data.begin(), file_data.end(),
+                                 [](const unsigned &c) { return !isspace(c) && !isalpha(c); }), file_data.end());
 
     auto finish_time = get_current_time_fenced();
     auto analyzing_time = get_current_time_fenced();
-    size_t data_portion_len = std::ceil(data.size() / num_threads * 2);
+    size_t data_portion_len = std::ceil(file_data.size() / num_threads * 2);
+
     // parallel counting
     std::vector<std::map<std::string, int>> vector_of_maps;
     t_queue<std::map<std::string, int>> queue_of_maps;
@@ -81,20 +89,23 @@ void parallel_count(const std::string &input_filename, const std::string &output
         std::map<std::string, int> map;
         vector_of_maps.push_back(map);
 
-        vector_of_threads.emplace_back(count_words, std::ref(data), i * data_portion_len,
+        vector_of_threads.emplace_back(count_words, std::ref(file_data), i * data_portion_len,
                                        std::min((i + 1) * data_portion_len, data.size()),
                                        std::ref(vector_of_maps[i]), std::ref(queue_of_maps));
     }
 
-    for (uint8_t i = std::floor(num_threads / 2); i < num_threads; i++) {
-        vector_of_threads.emplace_back(merge_maps_queue, std::ref(queue_of_maps));
+//    for (uint8_t i = std::floor(num_threads / 2); i < num_threads; i++) {
+//        vector_of_threads.emplace_back(merge_maps_queue, std::ref(queue_of_maps));
+//    }
+    std::map<std::string, int> map_of_all_words;
+    for (auto &map: vector_of_maps) {
+        map_of_all_words = merge_maps(map_of_all_words, map);
     }
-
     for (auto &t: vector_of_threads) {
         t.join();
     }
 
-    std::map<std::string, int> map_of_all_words = queue_of_maps.pop_back();
+//    std::map<std::string, int> map_of_all_words = queue_of_maps.pop_back();
     std::cout << "Loading: " << to_us(finish_time - total_time) << std::endl;
     std::cout << "Analyzing: " << to_us(get_current_time_fenced() - analyzing_time) << std::endl;
     print(map_of_all_words, output_filename_a, output_filename_n);
