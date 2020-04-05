06ac0cdeb9c243d592639f0e2d874af8082dbbdb
Mykola Morgunenko <Olli25010@gmail.com>
2020-04-05 15:02:53 +0300

    add linear program writing to files

--- count_number_of_all_words/includes/linear_program.h
+++ count_number_of_all_words/includes/linear_program.h
@@ -9,5 +9,6 @@
 
 void count_words(const std::string &input_filename, const std::string &output_filename_a,
                  const std::string &output_filename_n);
+std::string read_binary_file_into_buffer(const std::string& filename);
 
 #endif //COUNT_NUMBER_OF_ALL_WORDS_LINEAR_PROGRAM_H
