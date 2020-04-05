629ef6615dac5351aa6fc436e86bdc190bf8bbac
solka13Lenio <solomialenio13@gmail.com>
2020-04-05 14:23:52 +0300

    fixed parallel counting

--- count_number_of_all_words/includes/parallel_program.h
+++ count_number_of_all_words/includes/parallel_program.h
@@ -5,12 +5,14 @@
 #ifndef ARCHITECTURE_OF_COMPUTER_SYSTEMS_PARALLEL_PROGRAM_H
 #define ARCHITECTURE_OF_COMPUTER_SYSTEMS_PARALLEL_PROGRAM_H
 
-#ifndef COUNT_NUMBER_OF_ALL_WORDS_LINEAR_PROGRAM_H
-#define COUNT_NUMBER_OF_ALL_WORDS_LINEAR_PROGRAM_H
-
 #include <string>
+#include <vector>
+#include <map>
 
-void count_words(const std::string &input_filename, const std::string &output_filename_a,
-                 const std::string &output_filename_n, const int no_threads);
+void count_words(std::vector<std::string> &data, const int start_position, const int end_position,
+                 std::map<std::string, int> &map_of_words);
+
+void parallel_count(const std::string &input_filename, const std::string &output_filename_a,
+                    const std::string &output_filename_n, const int no_threads);
 
 #endif //ARCHITECTURE_OF_COMPUTER_SYSTEMS_PARALLEL_PROGRAM_H
--- count_number_of_all_words/src/parallel_program.cpp
+++ count_number_of_all_words/src/parallel_program.cpp
@@ -13,45 +13,13 @@
 #include <thread>
 
 //namespace locale=boost::locale;
-void parallel_count(const std::string &input_filename, const std::string &output_filename_a,
-                    const std::string &output_filename_n, const int no_threads){
-    // read entire binary archive into the buffer
-    std::ifstream raw_file(input_filename, std::ios::binary);
-    std::vector<std::string> data;
-
-    // read buffer
-    auto buffer = [&raw_file] {
-        std::ostringstream ss{};
-        ss << raw_file.rdbuf();
-        return ss.str();
-    }();
-    data = extract_to_memory(buffer);
-
-    // divide data
-    int data_len = data.size();
-    double tmp = data_len / no_threads;
-    int data_portion_len = static_cast<int>(tmp + 0.5 - (tmp < 0));
-
-    // parallel counting
-    std::vector<std::map<std::string, int>> vector_of_maps;
-    std::vector<std::thread> vector_of_threads;
-    for (size_t i = 0;  i < no_threads; i++) {
-        vector_of_maps.push_back(std::map<std::string, int>);
-        vector_of_threads.push_back(std::thread(count_words(&data, static_cast<int>i * data_portion_len,
-                std::min(static_cast<int>(i + 1) * data_portion_len, data_len), vector_of_maps[i])));
-    }
-    for (auto& t: vector_of_threads) {
-        t.join();
-    }
-    vector_of_threads.clear();
-}
-
-void count_words(const std::vector<std::string> &data, const int start_position, const int end_position,
-        std::map<std:string, int> &map_of_words) {
+void count_words(std::vector<std::string> &data, const int start_position, const int end_position,
+        std::map<std::string, int> &map_of_words) {
     std::string word;
 
-    for (vector_iter element = data.begin() + start_position; element != data.begin() + end_position) {
-//    for (auto &element : data.begin() + start_position) {
+    typedef std::vector<std::string>::iterator vec_iter;
+    for (vec_iter cur = data.begin() + start_position; cur != data.begin() + end_position; cur++) {
+        std::string element = *cur;
         element = boost::locale::to_lower(boost::locale::fold_case(boost::locale::normalize(element)));
         element.erase(std::remove_if(element.begin(), element.end(),
                                      [](const unsigned &c) { return !isspace(c) && !isalpha(c); }), element.end());
@@ -87,4 +55,40 @@ void count_words(const std::vector<std::string> &data, const int start_position,
 //    for_each(s.begin(), s.end(),
 //             [](std::string& x){ std::cout << x << std::endl; });
     // ##########################################################
+}
+
+void parallel_count(const std::string &input_filename, const std::string &output_filename_a,
+                    const std::string &output_filename_n, const int no_threads){
+    // read entire binary archive into the buffer
+    std::ifstream raw_file(input_filename, std::ios::binary);
+    std::vector<std::string> data;
+
+    // read buffer
+    auto buffer = [&raw_file] {
+        std::ostringstream ss{};
+        ss << raw_file.rdbuf();
+        return ss.str();
+    }();
+    extract_to_memory(buffer, &data);
+
+    // divide data
+    int data_len = data.size();
+    double tmp = data_len / no_threads;
+    int data_portion_len = static_cast<int>(tmp + 0.5 - (tmp < 0));
+
+    // parallel counting
+    std::vector<std::map<std::string, int>> vector_of_maps;
+    std::vector<std::thread> vector_of_threads;
+    for (size_t i = 0;  i < no_threads; i++) {
+        std::map<std::string, int> map;
+        vector_of_maps.push_back(map);
+
+        vector_of_threads.push_back(std::thread(count_words, std::ref(data), static_cast<int>(i) * data_portion_len,
+                                                            std::min(static_cast<int>(i + 1) * data_portion_len, data_len),
+                                                            std::ref(vector_of_maps[i])));
+    }
+    for (auto& t: vector_of_threads) {
+        t.join();
+    }
+    vector_of_threads.clear();
 }
