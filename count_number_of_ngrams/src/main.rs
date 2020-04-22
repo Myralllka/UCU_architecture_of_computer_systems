2919dc7c324022648d6e40f6ddd14365932379be
Mykola Morgunenko <Olli25010@gmail.com>
2020-04-22 17:48:47 +0300

    completed config

--- count_number_of_ngrams/src/main.rs
+++ count_number_of_ngrams/src/main.rs
@@ -1,22 +1,15 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
 extern crate ini;
-use ini::Ini;
 
-mod make_config;
+//use ini::Ini;
+mod config_file;
+
+use config_file::Config;
+use config_file::read_config_file;
+
 fn main() {
-    make_config::make_config_file();
-//    let conf = Ini::load_from_file("config.ini").unwrap();
-//
-//    let section = conf.section(Some("main")).unwrap();
-//    let threads = section.get("threads").unwrap();
-//    let green = section.get("infile").unwrap();
-//
-//    println!("{:?} {:?}", tommy, green);
-//
-//     iterating
-//    for (sec, prop) in &conf {
-//        println!("Section: {:?}", sec);
-//        for (key, value) in prop {
-//            println!("{:?}:{:?}", key, value);
-//        }
-//    }
+    let infile: String = std::env::args().nth(1).expect("no filename given");
+    let conf: Config = read_config_file(infile);
+    println!("{:?}", conf.threads);
 }
