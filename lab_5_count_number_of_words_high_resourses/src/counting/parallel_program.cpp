e6a5e4a35281c47cf40e566a2afb667ba90b8e50
fenix-125 <yurijpasichnyk11@gmail.com>
2020-05-17 16:23:13 +0300

    update map merging and add debug possibility

--- lab_5_count_number_of_words_high_resourses/src/counting/parallel_program.cpp
+++ lab_5_count_number_of_words_high_resourses/src/counting/parallel_program.cpp
@@ -7,23 +7,26 @@
 #include <thread>
 #include <boost/locale.hpp>
 #include "../../includes/archivation/archive_t.h"
+#include "../../includes/code_control.h"
 
 #include "tbb/concurrent_queue.h"
 
-#define DATAQ_CAPACITY 3
 namespace ba = boost::locale::boundary;
 
 void merge_maps(
         tbb::concurrent_bounded_queue<std::map<std::string, size_t>> &queue, uint8_t num_of_threads) {
-    for (; num_of_threads > 1; num_of_threads--) {
-        std::map<std::string, size_t> map1, map2;
-        queue.pop(map1);
-        queue.pop(map2);
-        for (auto &element:map1) {
-            map2[element.first] += map1[element.first];
+#ifdef DEBUG_INFO
+    std::cout << "\nStart Merge!" << std::endl;
+#endif
+    std::map<std::string, size_t> tmp_map, res_map{};
+    for (uint8_t map_id = 0; map_id < num_of_threads; ++map_id) {
+        queue.pop(tmp_map);
+        for (const auto &element : tmp_map) {
+            res_map[element.first] += tmp_map[element.first];
         }
-        queue.push(std::move(map2));
+        tmp_map.clear();
     }
+    queue.push(std::move(res_map));
 }
 
 static void counting(tbb::concurrent_bounded_queue<file_packet> &file_q,
@@ -34,7 +37,7 @@ static void counting(tbb::concurrent_bounded_queue<file_packet> &file_q,
     std::string tmp_content;
 
     while (true) {
-        while (!file_q.try_pop(packet)) {}
+        file_q.pop(packet);
         ////////////////// POISON PILL ///////////////////////
         if (packet.empty()) {
             file_q.push(file_packet());
@@ -52,7 +55,7 @@ static void counting(tbb::concurrent_bounded_queue<file_packet> &file_q,
         //////////////////////////////////////////////////////
 
         //////////////// INDEX EXTRACTED /////////////////////
-        while (data_q.empty()) {
+        while (!data_q.empty()) {
             tmp_content = std::move(data_q.front());
             data_q.pop_front();
             ba::ssegment_index map(ba::word, tmp_content.begin(), tmp_content.end());
@@ -62,10 +65,16 @@ static void counting(tbb::concurrent_bounded_queue<file_packet> &file_q,
                 map_of_words[word] += 1;
             }
             tmp_content.clear();
+#ifdef DEBUG_INFO
+            std::cout << "." << std::flush;
+#endif
         }
         //////////////////////////////////////////////////////
     }
     map_q.push(std::move(map_of_words));
+#ifdef DEBUG_INFO
+    std::cout << "\nEnd Count!" << std::endl;
+#endif
 }
 
 void parallel_count(tbb::concurrent_bounded_queue<file_packet> &loader_queue,
@@ -74,15 +83,19 @@ void parallel_count(tbb::concurrent_bounded_queue<file_packet> &loader_queue,
     std::vector<std::thread> vector_of_threads{};
     tbb::concurrent_bounded_queue<std::map<std::string, size_t>> map_queue;
     std::map<std::string, size_t> result;
+
     /////////////////////////// UNARCHIVE & COUNT ///////////////////
     for (uint8_t i = 0; i < number_of_threads; i++)
         vector_of_threads.emplace_back(counting, std::ref(loader_queue), std::ref(map_queue));
     /////////////////////////////////////////////////////////////////
-    merge_maps(map_queue, number_of_threads);
 
     for (auto &t: vector_of_threads)
         t.join();
-    /////////////////////////////////////////////////////////////////
+
+#ifdef DEBUG_INFO
+    std::cout << "\nMap queue size: " << map_queue.size() << std::endl;
+#endif
+    merge_maps(map_queue, number_of_threads);
     map_queue.pop(result);
     dump_map_to_files(result, output_filename_a, output_filename_n);
 }
